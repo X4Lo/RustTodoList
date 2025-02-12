15590e184c9c3e9b4b804d50c16777ba0b60610f
X4Lo <test52308@gmail.com>
2025-02-12 18:23:19 +0100

    add command-line mode

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+use std::env;
 use std::fmt;
 use std::fs;
 use std::io;
-use std::process::exit;
 
-static TASKS_FILE_PATH: &str =
-    "file path";
+static TASKS_FILE_PATH: &str = "file path";
 
 struct Task {
     id: u32,
@@ -30,6 +29,17 @@ impl fmt::Display for TaskStatus {
     }
 }
 
+impl TaskStatus {
+    fn from_str(status: &str) -> Option<TaskStatus> {
+        match status.trim().to_lowercase().as_str() {
+            "todo" => Some(TaskStatus::TODO),
+            "doing" => Some(TaskStatus::DOING),
+            "done" => Some(TaskStatus::DONE),
+            _ => None,
+        }
+    }
+}
+
 fn read_file() -> Result<Vec<Task>, io::Error> {
     let contents = fs::read_to_string(TASKS_FILE_PATH)?;
     let mut tasks: Vec<Task> = Vec::new();
@@ -38,22 +48,11 @@ fn read_file() -> Result<Vec<Task>, io::Error> {
         let v: Vec<&str> = line.split(';').collect();
 
         if v.len() < 3 {
-            eprintln!("Skipping malformed line: {}", line);
+            eprintln!("Skipping malformed line: '{}'", line);
             continue;
         }
 
-        println!("Raw status: {:?}", v[2]); // Debug output
-
-        let status = match v[2].trim() {
-            TODO => TaskStatus::TODO,
-            DOING => TaskStatus::DOING,
-            DONE => TaskStatus::DONE,
-            _ => {
-                eprintln!("Invalid status: '{}'", v[2]);
-                panic!("Invalid status");
-            }
-        };
-
+        let status = TaskStatus::from_str(v.get(2).unwrap()).unwrap();
         let task = Task {
             id: v.get(0).unwrap().parse::<u32>().unwrap(),
             title: v.get(1).unwrap().to_string(),
@@ -67,16 +66,18 @@ fn read_file() -> Result<Vec<Task>, io::Error> {
 }
 
 fn save_file(tasks: &Vec<Task>) {
-    let mut content: String = String::new();
+    let content: String = tasks
+        .iter()
+        .map(|task| format!("{};{};{}\n", task.id, task.title, task.status))
+        .collect::<String>()
+        .strip_suffix("\n")
+        .unwrap()
+        .to_string();
 
-    for task in tasks.iter() {
-        content.push_str(&format!("{};{};{}\n", task.id, task.title, task.status));
-    }
-
-    fs::write(TASKS_FILE_PATH, content);
+    let _ = fs::write(TASKS_FILE_PATH, content);
 }
 
-fn add_task(tasks: &mut Vec<Task>) {
+fn add_task_menu(tasks: &mut Vec<Task>) {
     println!("----------------------------------------------");
     println!("------------------ Add Task ------------------");
     println!("----------------------------------------------");
@@ -88,8 +89,16 @@ fn add_task(tasks: &mut Vec<Task>) {
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    let id = tasks[tasks.len() - 1].id + 1;
     let title = input.trim().to_string();
+
+    add_task(tasks, title);
+
+    println!("Task Added!");
+    println!("----------------------------------------------");
+}
+
+fn add_task(tasks: &mut Vec<Task>, title: String) {
+    let id = tasks[tasks.len() - 1].id + 1;
 
     tasks.push(Task {
         id: id,
@@ -98,11 +107,9 @@ fn add_task(tasks: &mut Vec<Task>) {
     });
 
     save_file(tasks);
-    println!(">>>>> Task Added! <<<<<");
-    println!("----------------------------------------------");
 }
 
-fn update_task(tasks: &mut Vec<Task>) {
+fn update_task_menu(tasks: &mut Vec<Task>) {
     println!("----------------------------------------------");
     println!("----------------- Update Task ----------------");
     println!("----------------------------------------------");
@@ -125,7 +132,7 @@ fn update_task(tasks: &mut Vec<Task>) {
     }
 
     if index.is_none() {
-        println!(">>>>> Task ID Not Found! <<<<<");
+        eprintln!("Task ID not found.");
     } else {
         println!("* - Available Status:");
         println!("1 - TODO");
@@ -154,13 +161,25 @@ fn update_task(tasks: &mut Vec<Task>) {
         tasks[index.unwrap()].status = status;
 
         save_file(tasks);
-        println!(">>>>> Task Updated! <<<<<");
+        println!("Task updated!");
     }
 
     println!("----------------------------------------------");
 }
 
-fn remove_task(tasks: &mut Vec<Task>) {
+fn update_task(tasks: &mut Vec<Task>, id: u32, status: TaskStatus) {
+    for task in tasks.iter_mut() {
+        if task.id == id {
+            task.status = status;
+            println!("Task updated!");
+            save_file(tasks);
+            return;
+        }
+    }
+    eprintln!("Task with ID: {} not found.", id);
+}
+
+fn remove_task_menu(tasks: &mut Vec<Task>) {
     println!("----------------------------------------------");
     println!("----------------- Remove Task ----------------");
     println!("----------------------------------------------");
@@ -174,26 +193,22 @@ fn remove_task(tasks: &mut Vec<Task>) {
 
     let id = input.trim().parse::<u32>().unwrap();
 
-    let mut index: Option<usize> = None;
-    for (i, task) in tasks.iter().enumerate() {
-        if task.id == id {
-            index = Some(i);
-            break;
-        }
-    }
-
-    if index.is_none() {
-        println!(">>>>> Task ID Not Found! <<<<<");
-    } else {
-        tasks.remove(index.unwrap());
-        save_file(tasks);
-        println!(">>>>> Task Removed! <<<<<");
-    }
+    remove_task(tasks, id);
 
     println!("----------------------------------------------");
 }
 
-fn view_tasks(tasks: &Vec<Task>) {
+fn remove_task(tasks: &mut Vec<Task>, id: u32) {
+    if let Some(index) = tasks.iter().position(|task| task.id == id) {
+        tasks.remove(index);
+        save_file(tasks);
+        println!("Task removed!")
+    } else {
+        eprintln!("Task with ID: {} not found.", id);
+    }
+}
+
+fn view_tasks_menu(tasks: &Vec<Task>) {
     println!("----------------------------------------------");
     println!("-------------- Your Tasks Are ----------------");
     println!("----------------------------------------------");
@@ -209,45 +224,76 @@ fn view_tasks(tasks: &Vec<Task>) {
 }
 
 fn main() {
-    let mut tasks: Vec<Task> = Vec::new();
-    let mut input = String::new();
-    let mut choice: u32 = 99;
+    let args: Vec<String> = env::args().collect();
+    let mut tasks = read_file().unwrap_or_else(|_| Vec::new());
 
-    match read_file() {
-        Ok(t) => tasks = t,
-        Err(e) => println!("Error reading file: {}", e),
-    }
-
-    println!("----------------------------------------------");
-    while choice != 0 {
-        println!("--------- Welcome to your TODO List ----------");
+    // interractive mode
+    if args.len() < 2 {
+        let mut input = String::new();
         println!("----------------------------------------------");
-        println!("1 - View Tasks");
-        println!("2 - Add Task");
-        println!("3 - Update Task");
-        println!("4 - Delete Task");
-        println!("0 - Quit");
-        println!("# - Choice > ");
+        loop {
+            println!("--------- Welcome to your TODO List ----------");
+            println!("----------------------------------------------");
+            println!("1 - View Tasks");
+            println!("2 - Add Task");
+            println!("3 - Update Task");
+            println!("4 - Delete Task");
+            println!("0 - Quit");
+            println!("# - Choice > ");
 
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-        let choice = input.trim().parse();
+            io::stdin().read_line(&mut input).unwrap();
 
-        match choice {
-            Ok(num) => println!("You entered: {}", num),
-            Err(_) => eprintln!("Invalid input! Please enter a valid number."),
+            match input.trim() {
+                "1" => view_tasks_menu(&tasks),
+                "2" => add_task_menu(&mut tasks),
+                "3" => update_task_menu(&mut tasks),
+                "4" => remove_task_menu(&mut tasks),
+                "0" => break,
+                _ => eprintln!("Invalid Choice!"),
+            }
+
+            input.clear();
         }
+    } else {
+        // command-line mode
+        match args[1].as_str() {
+            "--list" | "--l" => {
+                view_tasks_menu(&tasks);
+            }
+            "--add" | "--a" => {
+                if args.len() < 3 {
+                    eprintln!("Usage: --add \"task title\"");
+                    return;
+                }
 
-        input.clear();
+                let title: String = args[2..].join(" ").to_string();
+                add_task(&mut tasks, title);
+            }
+            "--update" | "--u" => {
+                if args.len() < 4 {
+                    eprintln!("Usage: --update <id> <status>");
+                    return;
+                }
 
-        match choice.unwrap() {
-            1 => view_tasks(&tasks),
-            2 => add_task(&mut tasks),
-            3 => update_task(&mut tasks),
-            4 => remove_task(&mut tasks),
-            0 => exit(0),
-            _ => println!("Invalid Choice!"),
+                let id: u32 = args[2].parse().expect("Invalid Task ID.");
+
+                if let Some(status) = TaskStatus::from_str(&args[3]) {
+                    update_task(&mut tasks, id, status);
+                } else {
+                    eprintln!("Invalid status!");
+                }
+            }
+            "--remove" | "--r" => {
+                if args.len() < 3 {
+                    eprintln!("Usage: --update <id> <status>");
+                    return;
+                }
+
+                let id: u32 = args[2].parse().expect("Invalid Task ID.");
+
+                remove_task(&mut tasks, id);
+            }
+            _ => eprintln!("Invalid arguments."),
         }
     }
 }
